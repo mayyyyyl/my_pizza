e8af52f7c2b960747ccd52b3a3e5753db751f869
Maylis Le Borgne <maylislb@hotmail.fr>
2021-09-28 16:15:46 +0200

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'my pizza': {
+        'ingredients': {'mozzarella': 3, 'parmesan': 3, 'basil': 2, 'garlic':1},
+        'price': 10
     }
 }
